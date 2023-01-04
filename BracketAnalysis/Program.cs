1342f33a50237857b1b18c779297e752420dfcb7
AmirHossein Sadeghi <amirhosseinsadeghy1380@gmail.com>
2023-01-04 19:01:08 +0330

    Make the Solution Class.

--- BracketAnalysis/Program.cs
+++ BracketAnalysis/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using BracketAnalysis;
+
+Solution solution = new Solution();
+while (true)
+{
+    Console.WriteLine("Please Enter the Brackets");
+    string str = Console.ReadLine();
+    Console.WriteLine($"Input : {str}\r\nOutput : {solution.IsValid(str)}");
+}
---
+++ BracketAnalysis/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketAnalysis
+{
+    public class Solution
+    {
+
+        public bool IsValid(string s)
+        {
+            while (s.IndexOfAny(new char[] { '(', '{', '[', ')', '}', ']' }) != -1)
+            {
+                if (s.Length == 1)
+                    return false;
+                for (int i = 1; i < s.Length; i++)
+                {
+                    if ((s[i - 1] == '(' && s[i] == ')') || (s[i - 1] == '{' && s[i] == '}') || (s[i - 1] == '[' && s[i] == ']'))
+                    {
+                        s = s.Remove(i - 1, 2);
+                        //break;
+                        return IsValid(s);
+                    }
+                    else if (i == s.Length - 1)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
